1145255679f68c6a6e8c52cc3f5727cee5e6b47b
benjamin.haverbeke <benjamin.haverbeke@outlook.com>
2024-03-28 14:12:06 +0000

    1511

---
+++ Ben/js/index.js
@@ -0,0 +1,30 @@
+/*********animatiton grenouille *********/
+
+/*
+
+document.addEventListener('DOMContentLoaded', function() {
+    let rainbow = document.querySelector('.rainbow-menu');
+    let frog = document.querySelector('.frog');
+    console.log(frog);
+    console.log(rainbow);
+
+const translate = [
+  
+  { transform: "translateY(-100%)", "endDelay: 30s" },
+  
+];
+    frog.addEventListener('mouseover', function() {
+
+            rainbow.animate(translate, 1000, "forwards");
+
+
+            });
+
+
+    });
+
+*/
+
+
+
+
